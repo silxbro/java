525c80f782c5fb6fe47458505e35454409ca739e
Park Eunhyeong <silbroeh@gmail.com>
2023-09-13 23:03:49 +0900

    feat : upload exam20

---
+++ src/thisisjava/ch03/SignOperatorExample.java
@@ -0,0 +1,13 @@
+package thisisjava.ch03;
+
+public class SignOperatorExample {
+  public static void main(String[] args) {
+    int x = -100;
+    x = -x;
+    System.out.println("x: " + x);  // x: 100
+
+    byte b = 100;
+    int y = -b;
+    System.out.println("y: " + y);  // y: -100
+  }
+}
