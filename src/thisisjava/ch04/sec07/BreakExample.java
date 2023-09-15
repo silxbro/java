145cfc1741419a87198404344939191b0d3f6739
Park Eunhyeong <silbroeh@gmail.com>
2023-09-15 14:15:17 +0900

    feat : upload exam52

---
+++ src/thisisjava/ch04/sec07/BreakExample.java
@@ -0,0 +1,24 @@
+package thisisjava.ch04.sec07;
+
+public class BreakExample {
+  public static void main(String[] args) throws Exception {
+    while (true) {
+      int num = (int)(Math.random() * 6) + 1;
+      System.out.println(num);
+      if (num == 6) {
+        break;
+      }
+    }
+    System.out.println("프로그램 종료");
+
+    /*
+
+    5
+    2
+    5
+    6
+    프로그램 종료
+
+    */
+  }
+}
