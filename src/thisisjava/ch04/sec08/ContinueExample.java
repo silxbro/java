a8394bec8647d43943156ed12afd24cf9924d0d6
Park Eunhyeong <silbroeh@gmail.com>
2023-09-15 14:37:15 +0900

    feat : upload exam54

---
+++ src/thisisjava/ch04/sec08/ContinueExample.java
@@ -0,0 +1,13 @@
+package thisisjava.ch04.sec08;
+
+public class ContinueExample {
+  public static void main(String[] args) throws Exception {
+    for (int i=1; i<=10; i++) {
+      if (i%2 != 0) {             // 2로 나눈 나머지가 0이 아닐 경우, 즉 홀수인 경우
+        continue;
+      }
+      System.out.print(i + " ");  // 홀수는 실행되지 않는다.
+    }
+    // 2 4 6 8 10
+  }
+}
