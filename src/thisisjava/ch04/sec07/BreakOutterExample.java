19d6a1acfe048db4e3ce8a104b8c14a5f70de8e3
Park Eunhyeong <silbroeh@gmail.com>
2023-09-15 14:24:46 +0900

    feat : upload exam53

---
+++ src/thisisjava/ch04/sec07/BreakOutterExample.java
@@ -0,0 +1,26 @@
+package thisisjava.ch04.sec07;
+
+public class BreakOutterExample {
+  public static void main(String[] args) throws Exception {
+    Outter: for (char upper = 'A'; upper <= 'Z'; upper++) {
+      for (char lower = 'a'; lower <= 'z'; lower++) {
+        System.out.println(upper + "-" + lower);
+        if (lower == 'g') {
+          break Outter;
+        }
+      }
+    }
+    System.out.println("프로그램 실행 종료");
+
+    /*
+    A-a
+    A-b
+    A-c
+    A-d
+    A-e
+    A-f
+    A-g
+    프로그램 실행 종료
+     */
+  }
+}
