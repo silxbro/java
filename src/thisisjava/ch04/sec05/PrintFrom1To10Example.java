08913cc4ee45424c169802b74940034242aff3c0
Park Eunhyeong <silbroeh@gmail.com>
2023-09-15 12:15:01 +0900

    feat : upload exam48

---
+++ src/thisisjava/ch04/sec05/PrintFrom1To10Example.java
@@ -0,0 +1,12 @@
+package thisisjava.ch04.sec05;
+
+public class PrintFrom1To10Example {
+  public static void main(String[] args) {
+    int i = 1;
+    while (i<=10) {
+      System.out.print(i + " ");
+      i++;
+    }
+    // 1 2 3 4 5 6 7 8 9 10
+  }
+}
