3a437963a3fc75d57083f1a359b2793c16ad82c1
Park Eunhyeong <silbroeh@gmail.com>
2023-09-15 10:06:58 +0900

    feat : upload exam35

---
+++ src/thisisjava/ch04/IfElseExample.java
@@ -0,0 +1,18 @@
+package thisisjava.ch04;
+
+public class IfElseExample {
+  public static void main(String[] args) {
+    int score = 85;
+
+    if (score >= 90) {
+      System.out.println("점수가 90보다 큽니다.");
+      System.out.println("등급은 A입니다.");
+    } else {    // ---> score < 90 일 경우
+      System.out.println("점수가 90보다 작습니다.");
+      System.out.println("등급은 B입니다.");
+    }
+
+    // 점수가 90보다 작습니다.
+    // 등급은 B입니다.
+  }
+}
