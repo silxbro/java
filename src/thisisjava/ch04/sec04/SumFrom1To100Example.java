dee2fed315a95a9e283aa3a534769bb26d55dde3
Park Eunhyeong <silbroeh@gmail.com>
2023-09-15 11:55:50 +0900

    feat : upload exam45

---
+++ src/thisisjava/ch04/sec04/SumFrom1To100Example.java
@@ -0,0 +1,14 @@
+package thisisjava.ch04.sec04;
+
+public class SumFrom1To100Example {
+  public static void main(String[] args) {
+    int sum = 0;    // 합계 변수
+    int i;          // 카운터 변수
+
+    for (i=1; i<=100; i++) {
+      sum += i;
+    }
+
+    System.out.println("1~" + (i-1) + " 합 : " + sum);  // 1~100 합 : 5050
+  }
+}
