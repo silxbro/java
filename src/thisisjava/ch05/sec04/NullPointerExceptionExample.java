e5330dfd89c96feeb7be0ba3749a83933ab9cfce
Park Eunhyeong <silbroeh@gmail.com>
2023-09-15 15:49:53 +0900

    feat : upload exam57

--- src/thisisjava/ch05/sec04/NullPointerExceptionExample.java
+++ src/thisisjava/ch05/sec04/NullPointerExceptionExample.java
@@ -5,7 +5,17 @@ public class NullPointerExceptionExample {
     int[] intArray = null;
     //intArray[0] = 10;   // NullPointerException
 
+    /*
+    Exception in thread "main" java.lang.NullPointerException: Cannot store to int array because "intArray" is null
+	  at thisisjava.ch05.sec04.NullPointerExceptionExample.main(NullPointerExceptionExample.java:6)
+     */
+
     String str = null;
-    //System.out.println("총 문자 수: " + str.length());  // NullPointerException
+    System.out.println("총 문자 수: " + str.length());  // NullPointerException
+
+    /*
+    Exception in thread "main" java.lang.NullPointerException: Cannot invoke "String.length()" because "str" is null
+	  at thisisjava.ch05.sec04.NullPointerExceptionExample.main(NullPointerExceptionExample.java:14)
+     */
   }
 }
