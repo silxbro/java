adb1340ecc8b47f39caf311cd16dcf2f8b51a18d
Park Eunhyeong <silbroeh@gmail.com>
2023-09-16 13:26:03 +0900

    style : add space exam73-line12

--- src/thisisjava/ch05/sec08/ArrayReferenceObjectExample.java
+++ src/thisisjava/ch05/sec08/ArrayReferenceObjectExample.java
@@ -9,6 +9,6 @@ public class ArrayReferenceObjectExample {
 
     System.out.println( strArray[0] == strArray[1] );     // true : 같은 객체 참조
     System.out.println( strArray[0] == strArray[2] );     // false : 다른 객체 참조
-    System.out.println( strArray[0].equals(strArray[2])); // true : 문자열이 동일
+    System.out.println( strArray[0].equals(strArray[2]) ); // true : 문자열이 동일
   }
 }
