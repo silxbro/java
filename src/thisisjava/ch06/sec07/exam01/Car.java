068cfac60bcac40df99c4c111622d227397c2c36
Park Eunhyeong <silbroeh@gmail.com>
2023-09-18 20:03:11 +0900

    feat : upload exam83

---
+++ src/thisisjava/ch06/sec07/exam01/Car.java
@@ -0,0 +1,10 @@
+package thisisjava.ch06.sec07.exam01;
+
+public class Car {
+  // 생성자 선언
+  Car(String model, String color, int maxSpeed) {
+  }
+  /*
+   클래스에 개발자가 선언한 생성자가 있다면 컴파일러는 기본 생성자를 추가하지 않는다.
+   */
+}
