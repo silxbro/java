37889bd1939233de381cce5884773f2feb91a70f
Park Eunhyeong <silbroeh@gmail.com>
2023-09-19 13:58:49 +0900

    feat : upload exam98

---
+++ src/thisisjava/ch06/sec11/exam02/EarthExample.java
@@ -0,0 +1,9 @@
+package thisisjava.ch06.sec11.exam02;
+
+public class EarthExample {
+  public static void main(String[] args) {
+    // 상수 읽기
+    System.out.println("지구의 반지름: " + Earth.EARTH_RADIUS + "km");         // 지구의 반지름: 6400.0km
+    System.out.println("지구의 표면적: " + Earth.EARTH_SURFACE_AREA + "km^2"); // 지구의 표면적: 5.147185403641517E8km^2
+  }
+}
