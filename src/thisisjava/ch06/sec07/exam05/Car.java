337962c3417ae9d839185383b6112a667276c6cc
Park Eunhyeong <silbroeh@gmail.com>
2023-09-19 09:41:01 +0900

    feat : upload exam87

---
+++ src/thisisjava/ch06/sec07/exam05/Car.java
@@ -0,0 +1,25 @@
+package thisisjava.ch06.sec07.exam05;
+
+public class Car {
+  // 필드 선언
+  String company = "현대자동차";
+  String model;
+  String color;
+  int maxSpeed;
+
+  Car(String model) {                              // ① 생성자
+    // 20라인 생성자 호출
+    this(model, "은색", 250);
+  }
+
+  Car(String model, String color) {                // ② 생성자
+    // 20라인 생성자 호출
+    this(model, color, 250);
+  }
+
+  Car(String model, String color, int maxSpeed) {  // ③ 생성자
+    this.model = model;
+    this.color = color;
+    this.maxSpeed = maxSpeed;
+  }
+}
