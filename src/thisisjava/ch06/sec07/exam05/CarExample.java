bb008c2acfb06c05d6624d3bc573016c495b6cb8
Park Eunhyeong <142463332+silxbro@users.noreply.github.com>
2023-09-19 09:43:02 +0900

    comment : set line

--- src/thisisjava/ch06/sec07/exam05/CarExample.java
+++ src/thisisjava/ch06/sec07/exam05/CarExample.java
@@ -7,13 +7,13 @@ public class CarExample {
     System.out.println("car1.model : " + car1.model);
     System.out.println();
 
-    Car car2 = new Car("자가용", "빨강");              // ② 생성자 호출
+    Car car2 = new Car("자가용", "빨강");                    // ② 생성자 호출
     System.out.println("car2.company : " + car2.company);
     System.out.println("car2.model : " + car2.model);
     System.out.println("car2.color : " + car2.color);
     System.out.println();
 
-    Car car3 = new Car("택시", "검정", 200); // ③ 생성자 호출
+    Car car3 = new Car("택시", "검정", 200);                 // ③ 생성자 호출
     System.out.println("car3.company : " + car3.company);
     System.out.println("car3.model : " + car3.model);
     System.out.println("car3.color : " + car3.color);
