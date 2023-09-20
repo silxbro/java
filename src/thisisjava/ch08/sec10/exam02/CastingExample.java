096071943f15ecd74d5a0e23defc74de7ba2c264
Park Eunhyeong <silbroeh@gmail.com>
2023-09-21 08:51:03 +0900

    feat : upload exam140

---
+++ src/thisisjava/ch08/sec10/exam02/CastingExample.java
@@ -0,0 +1,23 @@
+package thisisjava.ch08.sec10.exam02;
+
+public class CastingExample {
+  public static void main(String[] args) {
+    // 인터페이스 변수 선언과 구현 객체 대입
+    Vehicle vehicle = new Bus();
+
+    // 인터페이스를 통해서 호출
+    vehicle.run();
+    //vehicle.checkFare();
+
+    // 강제 타입 변환 후 호출
+    Bus bus = (Bus) vehicle;
+    bus.run();
+    bus.checkFare();
+
+    /*
+    버스가 달립니다.
+    버스가 달립니다.
+    승차요금을 체크합니다.
+     */
+  }
+}
