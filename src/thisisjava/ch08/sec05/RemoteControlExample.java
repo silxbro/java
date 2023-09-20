fb48071e7b007c01d728754f7fa7f3cc7ef63345
Park Eunhyeong <silbroeh@gmail.com>
2023-09-20 17:56:06 +0900

    feat : upload exam133

--- src/thisisjava/ch08/sec05/RemoteControlExample.java
+++ src/thisisjava/ch08/sec05/RemoteControlExample.java
@@ -22,5 +22,26 @@ public class RemoteControlExample {
     현재 TV 볼륨: 0
     무음 해제합니다.
      */
+
+    System.out.println();
+
+    // Audio 객체를 생성하고 인터페이스 변수에 대입
+    rc = new Audio();
+    rc.turnOn();
+    rc.setVolume(5);
+    /*
+    Audio를 켭니다.
+    현재 Audio 볼륨: 5
+     */
+
+    // 디폴트 메소드 호출
+    rc.setMute(true);
+    rc.setMute(false);
+    /*
+    무음 처리합니다.
+    현재 Audio 볼륨: 0
+    무음 해제합니다.
+    현재 Audio 볼륨: 5 ---> 복원된 볼륨
+     */
   }
 }
