cfb053ab55b8cad2d83035d332cf0b9f919b9d13
Park Eunhyeong <silbroeh@gmail.com>
2023-09-20 17:44:30 +0900

    feat : upload exam132

---
+++ src/thisisjava/ch08/sec05/RemoteControlExample.java
@@ -0,0 +1,26 @@
+package thisisjava.ch08.sec05;
+
+public class RemoteControlExample {
+  public static void main(String[] args) {
+    // 인터페이스 변수 선언
+    RemoteControl rc;
+
+    // Television 객체를 생성하고 인터페이스 변수에 대입
+    rc = new Television();
+    rc.turnOn();
+    rc.setVolume(5);
+    /*
+    TV를 켭니다.
+    현재 TV 볼륨: 5
+     */
+
+    // 디폴트 메소드 호출
+    rc.setMute(true);
+    rc.setMute(false);
+    /*
+    무음 처리합니다.
+    현재 TV 볼륨: 0
+    무음 해제합니다.
+     */
+  }
+}
