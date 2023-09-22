6e1257061fc20466b51e53d70b1584f057457ea8
Park Eunhyeong <silbroeh@gmail.com>
2023-09-22 14:05:17 +0900

    feat : upload exam164

---
+++ src/thisisjava/ch11/sec02/exam01/ExceptionHandlingExample2.java
@@ -0,0 +1,36 @@
+package thisisjava.ch11.sec02.exam01;
+
+public class ExceptionHandlingExample2 {
+  public static void printLength(String data) {
+    try {
+      int result = data.length();
+      System.out.println("문자 수: " + result);
+    } catch (NullPointerException e) {
+      // 예외 정보를 얻는 3가지 방법
+      System.out.println(e.getMessage());  // 1
+      //System.out.println(e.toString());  // 2
+      //e.printStackTrace();               // 3
+    } finally {
+      System.out.println("[마무리 실행]\n");
+    }
+  }
+
+  public static void main(String[] args) {
+    System.out.println("[프로그램 시작]\n");
+    printLength("ThisIsJava");
+    printLength(null);
+    System.out.println("[프로그램 종료]");
+
+    /*
+    [프로그램 시작]
+
+    문자 수: 10
+    [마무리 실행]
+
+    Cannot invoke "String.length()" because "data" is null
+    [마무리 실행]
+
+    [프로그램 종료]
+     */
+  }
+}
