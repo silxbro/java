b340665ec9cc0d1062ccf35908bbb4ef07bd70cc
Park Eunhyeong <silbroeh@gmail.com>
2023-09-23 17:15:10 +0900

    feat : upload exam177

---
+++ src/thisisjava/ch12/sec03/exam03/SmartPhone.java
@@ -0,0 +1,16 @@
+package thisisjava.ch12.sec03.exam03;
+
+public class SmartPhone {
+  private String company;
+  private String os;
+  
+  public SmartPhone(String company, String os) {
+    this.company = company;
+    this.os = os;
+  }
+  
+  @Override
+  public String toString() {     // Object의 toString() 메소드를 재정의해서 제조사와 운영체제가 결합된 문자열을 리턴하도록 함
+    return company + ", " + os;
+  }
+}
---
+++ src/thisisjava/ch12/sec03/exam03/ToStringExample.java
@@ -0,0 +1,14 @@
+package thisisjava.ch12.sec03.exam03;
+
+public class ToStringExample {
+  public static void main(String[] args) {
+    SmartPhone myPhone = new SmartPhone("삼성전자", "안드로이드");
+    
+    String strObj = myPhone.toString();  // toString() 메소드 호출
+    System.out.println(strObj);
+    // 삼성전자, 안드로이드
+    
+    System.out.println(myPhone);         // toString() 메소드 호출
+    // 삼성전자, 안드로이드
+  }
+}
