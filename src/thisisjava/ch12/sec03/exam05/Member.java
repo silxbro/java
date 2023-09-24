f00aa31b2aca0fdf194db3bd66c2c78b73bb8116
Park Eunhyeong <silbroeh@gmail.com>
2023-09-24 23:39:33 +0900

    feat : upload exam181

--- src/thisisjava/ch12/sec03/exam05/Member.java
+++ src/thisisjava/ch12/sec03/exam05/Member.java
@@ -1,14 +1,11 @@
 package thisisjava.ch12.sec03.exam05;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Member {
   private String id;
-  private String name;
+  @NonNull private String name;
   private int age;
 }
