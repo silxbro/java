d66455e4848bb39248a235f8a6e5640998534eb3
Park Eunhyeong <silbroeh@gmail.com>
2023-09-25 15:31:37 +0900

    feat : upload exam200

---
+++ src/thisisjava/ch12/sec09/DecimalFormatExample.java
@@ -0,0 +1,19 @@
+package thisisjava.ch12.sec09;
+
+import java.text.DecimalFormat;
+
+public class DecimalFormatExample {
+  public static void main(String[] args) {
+    double num = 1234567.89;
+
+    DecimalFormat df;
+
+    // 정수 자리까지 표기
+    df = new DecimalFormat("#,###");
+    System.out.println(df.format(num));  // 1,234,568
+
+    // 무조건 소수 첫째 자리까지 표기
+    df = new DecimalFormat("#,###.0");
+    System.out.println(df.format(num));  // 1,234,567.9
+  }
+}
