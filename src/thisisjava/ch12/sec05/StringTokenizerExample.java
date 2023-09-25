41f3cb84e6c5a49682658610e4cb49ee09c181c2
Park Eunhyeong <silbroeh@gmail.com>
2023-09-25 10:54:39 +0900

    feat : upload exam189

---
+++ src/thisisjava/ch12/sec05/StringTokenizerExample.java
@@ -0,0 +1,31 @@
+package thisisjava.ch12.sec05;
+
+import java.util.StringTokenizer;
+
+public class StringTokenizerExample {
+  public static void main(String[] args) {
+    String data1 = "홍길동&이수홍,박연수";
+    String[] arr1 = data1.split("&|,");
+    for (String token : arr1) {
+      System.out.println(token);
+    }
+    System.out.println();
+
+    String data2 = "홍길동/이수홍/박연수";
+    StringTokenizer st = new StringTokenizer(data2, "/");
+    while (st.hasMoreTokens()) {
+      String token = st.nextToken();
+      System.out.println(token);
+    }
+
+    /*
+    홍길동
+    이수홍
+    박연수
+
+    홍길동
+    이수홍
+    박연수
+     */
+  }
+}
