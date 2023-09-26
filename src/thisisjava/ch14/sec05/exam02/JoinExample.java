acf53abb5f46d3d13af80aa170f7e18331dcbf74
Park Eunhyeong <silbroeh@gmail.com>
2023-09-26 14:46:33 +0900

    feat : upload exam224

---
+++ src/thisisjava/ch14/sec05/exam02/JoinExample.java
@@ -0,0 +1,13 @@
+package thisisjava.ch14.sec05.exam02;
+
+public class JoinExample {
+  public static void main(String[] args) {
+    SumThread sumThread = new SumThread();
+    sumThread.start();
+    try {
+      sumThread.join();
+    } catch (InterruptedException e) {
+    }
+    System.out.println("1~100 합: " + sumThread.getSum());  // 1~100 합: 5050
+  }
+}
