a6c7474bfd0d176ca0a2bb8e1172d0d5aaf94844
Park Eunhyeong <silbroeh@gmail.com>
2023-10-01 17:57:58 +0900

    feat : upload exam249

---
+++ src/thisisjava/ch15/sec05/exam03/Person.java
@@ -0,0 +1,18 @@
+package thisisjava.ch15.sec05.exam03;
+
+public class Person implements Comparable<Person> {
+  public String name;
+  public int age;
+
+  public Person(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  @Override
+  public int compareTo(Person o) {
+    if (age<o.age) return -1;
+    else if (age>o.age) return 1;
+    else return 0;
+  }
+}
