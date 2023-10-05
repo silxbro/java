623fa0e2ca2a6b9fd4be5c7451232316669b43ee
Park Eunhyeong <silbroeh@gmail.com>
2023-10-05 10:01:58 +0900

    feat : upload exam272

---
+++ src/thisisjava/ch17/sec06/exam02/MapExample.java
@@ -0,0 +1,36 @@
+package thisisjava.ch17.sec06.exam02;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class MapExample {
+  public static void main(String[] args) {
+    int[] intArray = { 1, 2, 3, 4, 5 };
+
+    IntStream intStream = Arrays.stream(intArray);
+    intStream
+        .asDoubleStream()
+        .forEach(d -> System.out.println(d));
+
+    System.out.println();
+
+    intStream = Arrays.stream(intArray);
+    intStream
+        .boxed()
+        .forEach(obj -> System.out.println(obj.intValue()));
+
+    /*
+    1.0
+    2.0
+    3.0
+    4.0
+    5.0
+
+    1
+    2
+    3
+    4
+    5
+     */
+  }
+}
