39c456d3907e2048ab954eed62d7f63b4c918b5c
Park Eunhyeong <silbroeh@gmail.com>
2023-10-18 12:27:01 +0900

    feat : upload exam301

---
+++ src/thisisjava/ch19/sec02/InetAddressExample.java
@@ -0,0 +1,26 @@
+package thisisjava.ch19.sec02;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class InetAddressExample {
+  public static void main(String[] args) {
+    try {
+      InetAddress local = InetAddress.getLocalHost();
+      System.out.println("내 컴퓨터 IP 주소: " + local.getHostAddress());
+
+      InetAddress[] iaArr = InetAddress.getAllByName("www.naver.com");
+      for (InetAddress remote : iaArr) {
+        System.out.println("www.naver.com IP 주소: " + remote.getHostAddress());
+      }
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+
+    /*
+    내 컴퓨터 IP 주소: 172.30.88.61
+    www.naver.com IP 주소: 223.130.200.104
+    www.naver.com IP 주소: 223.130.200.107
+     */
+  }
+}
