d92521cadd676de97a893394c9cb6c7d5cc58a50
Park Eunhyeong <silbroeh@gmail.com>
2023-10-19 15:26:34 +0900

    feat : upload exam310

---
+++ src/thisisjava/ch19/sec06/ParseJsonExample.java
@@ -0,0 +1,52 @@
+package thisisjava.ch19.sec06;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+public class ParseJsonExample {
+  public static void main(String[] args) throws IOException {
+    // 파일로부터 JSON 읽기
+    BufferedReader br = new BufferedReader(
+        new FileReader("C:/Temp/member.json", Charset.forName("UTF-8"))
+    );
+    String json = br.readLine();
+    br.close();
+
+    // JSON 파싱
+    JSONObject root = new JSONObject(json);
+
+    // 속성 정보 읽기
+    System.out.println("id: " + root.getString("id"));
+    System.out.println("name: " + root.getString("name"));
+    System.out.println("age: " + root.getInt("age"));
+    System.out.println("student: " + root.getBoolean("student"));
+
+    // 객체 속성 정보 읽기
+    JSONObject tel = root.getJSONObject("tel");
+    System.out.println("home: " + tel.getString("home"));
+    System.out.println("mobile: " + tel.getString("mobile"));
+
+    // 배열 속성 정보 읽기
+    JSONArray skill = root.getJSONArray("skill");
+    System.out.print("skill: ");
+    for (int i=0; i<skill.length(); i++) {
+      System.out.print(skill.get(i) + ", ");
+    }
+
+    /*
+    id: winter
+    name: 한겨울
+    age: 25
+    student: true
+    home: 01-123-1234
+    mobile: 010-123-1234
+    skill: java, C, C++,
+     */
+  }
+}
